96e11c07a2fb431b63338005053d0592c02bb9bc
EricRishiSingh <esingh@live.com>
2015-10-28 21:49:50 -0400

    Optimize code

--- StockAnalysis/Controllers/StockController.cs
+++ StockAnalysis/Controllers/StockController.cs
@@ -5,10 +5,8 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
-using System.Web;
-using System.Web.Mvc;
 using System.Web.Helpers;
+using System.Web.Mvc;
 using System.Xml.Serialization;
 
 namespace StockAnalysis.Controllers
@@ -17,26 +15,27 @@ namespace StockAnalysis.Controllers
     {
         #region Fields/Properties
 
+        public const string YahooURL = "http://download.finance.yahoo.com/d/quotes.csv?s=";
 
         #endregion
 
         //
         // GET: /Stocks/
 
-        public async Task<ActionResult> Stocks()
+        public ActionResult Stocks()
         {
             // Initialize stocks and stock info
             List<StockModel> stockModels = new List<StockModel>();
             List<string> stockSymbols = new List<string>() { "^GSPC", "^GSPTSE" };
 
-            using (var userContext = new UsersContext())
+            PerformDatabaseAction((userContext, user) =>
             {
                 // Add user stock to the table
-                var user = userContext.GetUser(User.Identity.Name);
-                if (user != null)
-                    if (user?.StockSymbols?.Any() ?? false)
-                        stockSymbols.AddRange(Deserialize<List<string>>(user.StockSymbols));
-            }
+                if (user?.StockSymbols?.Any() ?? false)
+                    stockSymbols.AddRange(Deserialize<List<string>>(user.StockSymbols));
+            });
+
+            #region Symbols
 
             // Get the stock info by using the following arguments
             // StockSymbol = s
@@ -48,50 +47,36 @@ namespace StockAnalysis.Controllers
             // Dividend = d
             // TargetPrice = t8
             // Name = n
-            string url = @"http://download.finance.yahoo.com/d/quotes.csv?s=" + string.Join("+", stockSymbols) + "&f=sarj1jkdt8n";
-            var result = await GetWebResponse(url);
+
+            #endregion
 
             // Retrieve stock information and add to list for view to use
-            using (StreamReader strm = new StreamReader(result.GetResponseStream()))
+            foreach (var line in ParseWebResponse(YahooURL + string.Join("+", stockSymbols) + "&f=sarj1jkdt8n"))
             {
-                string line;
-                while ((line = strm.ReadLine()) != null)
+                var lineInfo = line.Split(',');
+                stockModels.Add(new StockModel()
                 {
-                    var lineInfo = line.Split(',');
-                    stockModels.Add(new StockModel()
-                    {
-                        StockSymbol = lineInfo[0].Replace("\"", ""),
-                        StockPrice = lineInfo[1],
-                        PERatio = lineInfo[2],
-                        MarketCap = lineInfo[3],
-                        YearLow = lineInfo[4],
-                        YearHigh = lineInfo[5],
-                        Dividend = lineInfo[6],
-                        TargetPrice = lineInfo[7],
-                        Name = lineInfo[8].Replace("\"", "")
-                    });
-                }
+                    StockSymbol = lineInfo[0].Replace("\"", ""),
+                    StockPrice = lineInfo[1],
+                    PERatio = lineInfo[2],
+                    MarketCap = lineInfo[3],
+                    YearLow = lineInfo[4],
+                    YearHigh = lineInfo[5],
+                    Dividend = lineInfo[6],
+                    TargetPrice = lineInfo[7],
+                    Name = lineInfo[8].Replace("\"", "")
+                });
             }
 
             var userStockModels = new List<UserStockModel>();
-            using (var userContext = new UsersContext())
+            PerformDatabaseAction((userContext, user) =>
             {
-                if (User?.Identity?.IsAuthenticated ?? true)
-                {
-                    var user = userContext.GetUser(User.Identity.Name);
-                    if (user != null)
-                    {
-                        userStockModels = string.IsNullOrEmpty(user.UserStockInformation) ? new List<UserStockModel>() : Deserialize<List<UserStockModel>>(user.UserStockInformation);
+                userStockModels = string.IsNullOrEmpty(user.UserStockInformation) ? new List<UserStockModel>() : Deserialize<List<UserStockModel>>(user.UserStockInformation);
+                foreach (var stock in userStockModels)
+                    UpdateUserStocks(stock);
 
-                        foreach (var stock in userStockModels)
-                            await UpdateUserStocks(stock);
-
-                        user.UserStockInformation = Serialize<List<UserStockModel>>(userStockModels);
-                        userContext.Entry(user).CurrentValues.SetValues(user);
-                        userContext.SaveChanges();
-                    }
-                }
-            }
+                user.UserStockInformation = Serialize<List<UserStockModel>>(userStockModels);
+            });
 
             return View(new UserView() { StockModels = stockModels, UserStockModels = userStockModels });
         }
@@ -101,34 +86,24 @@ namespace StockAnalysis.Controllers
             if (!ValidateStockSymbol(stockSymbol))
                 return RedirectToAction("Stocks");
 
-            // Save result in database
-            using (var userContext = new UsersContext())
+            PerformDatabaseAction((userContext, user) =>
             {
-                if (User?.Identity?.IsAuthenticated ?? true)
-                {
-                    var user = userContext.GetUser(User.Identity.Name);
-                    if (user != null)
-                    {
-                        string retStockSymbol = stockSymbol.ToUpperInvariant();
-                        var list = new List<string>();
-                        if (user.StockSymbols != null)
-                            list = Deserialize<List<string>>(user.StockSymbols);
+                string retStockSymbol = stockSymbol.ToUpperInvariant();
+                var list = new List<string>();
+                if (user.StockSymbols != null)
+                    list = Deserialize<List<string>>(user.StockSymbols);
 
-                        if (!list.Contains(retStockSymbol))
-                        {
-                            list.Add(retStockSymbol);
-                            user.StockSymbols = Serialize<List<string>>(list);
-                            userContext.Entry(user).CurrentValues.SetValues(user);
-                            userContext.SaveChanges();
-                        }
-                    }
+                if (!list.Contains(retStockSymbol))
+                {
+                    list.Add(retStockSymbol);
+                    user.StockSymbols = Serialize<List<string>>(list);
                 }
-            }
+            });
 
             return RedirectToAction("Stocks");
         }
 
-        public async Task<ActionResult> AddUserStock(string stockSymbol, string numberOfShares, string stockPrice)
+        public ActionResult AddUserStock(string stockSymbol, string numberOfShares, string stockPrice)
         {
             float numOfShares = 0;
             float price = 0;
@@ -138,62 +113,51 @@ namespace StockAnalysis.Controllers
                 return RedirectToAction("Stocks");
 
             // Save result in database
-            // TODO find a better way to get the user - make it common
-            using (var userContext = new UsersContext())
+            PerformDatabaseAction((userContext, user) =>
             {
-                if (User?.Identity?.IsAuthenticated ?? true)
+                var userStocks = string.IsNullOrEmpty(user.UserStockInformation) ? new List<UserStockModel>() : Deserialize<List<UserStockModel>>(user.UserStockInformation);
+                var stock = userStocks.FirstOrDefault(i => i.StockSymbol == stockSymbol.ToUpperInvariant());
+                if (stock != null)
                 {
-                    var user = userContext.GetUser(User.Identity.Name);
-                    if (user != null)
+                    var stockPurchases = stock.StockPurchases;
+                    if (stockPurchases == null)
+                        stockPurchases = new List<StockPurchase>();
+
+                    if ((stockPurchases.Sum(i => i.NumberOfShares) + numOfShares) > 0)
                     {
-                        var userStocks = string.IsNullOrEmpty(user.UserStockInformation) ? new List<UserStockModel>() : Deserialize<List<UserStockModel>>(user.UserStockInformation);
-                        var stock = userStocks.FirstOrDefault(i => i.StockSymbol == stockSymbol.ToUpperInvariant());
-                        if (stock != null)
+                        if (price > 0)
+                            stockPurchases.Add(new StockPurchase() { NumberOfShares = numOfShares, StockPrice = price });
+
+                        UpdateUserStocks(stock);
+                    }
+                }
+                else
+                {
+                    if (price > 0)
+                    {
+                        var newStock = new UserStockModel()
                         {
-                            var stockPurchases = stock.StockPurchases;
-                            if (stockPurchases == null)
-                                stockPurchases = new List<StockPurchase>();
+                            StockSymbol = stockSymbol.ToUpperInvariant(),
+                            StockPurchases = new List<StockPurchase>() { new StockPurchase() { NumberOfShares = numOfShares, StockPrice = price } }
+                        };
 
-                            if ((stockPurchases.Sum(i => i.NumberOfShares) + numOfShares) < 0)
-                                return RedirectToAction("Stocks");
+                        if (price > 0)
+                            userStocks.Add(newStock);
 
-                            if (price > 0)
-                                stockPurchases.Add(new StockPurchase() { NumberOfShares = numOfShares, StockPrice = price });
-
-                            await UpdateUserStocks(stock);
-                        }
-                        else
-                        {
-                            if (price > 0)
-                            {
-                                var newStock = new UserStockModel()
-                                {
-                                    StockSymbol = stockSymbol.ToUpperInvariant(),
-                                    StockPurchases = new List<StockPurchase>() { new StockPurchase() { NumberOfShares = numOfShares, StockPrice = price } }
-                                };
-
-
-                                if (price > 0)
-                                    userStocks.Add(newStock);
-
-                                await UpdateUserStocks(newStock);
-                            }
-                        }
-
-                        user.UserStockInformation = Serialize<List<UserStockModel>>(userStocks);
-                        userContext.Entry(user).CurrentValues.SetValues(user);
-                        userContext.SaveChanges();
+                        UpdateUserStocks(newStock);
                     }
                 }
 
-                return RedirectToAction("Stocks");
-            }
+                user.UserStockInformation = Serialize<List<UserStockModel>>(userStocks);
+            });
+
+            return RedirectToAction("Stocks");
         }
 
         // TODO improve performance. This method called too many times
-        private async Task UpdateUserStocks(UserStockModel stock)
+        private void UpdateUserStocks(UserStockModel stock)
         {
-            float stockPrice = await GetStockPrice(stock.StockSymbol);
+            float stockPrice = GetStockPrice(stock.StockSymbol);
 
             if (stock?.StockPurchases?.Any() == true)
             {
@@ -223,58 +187,51 @@ namespace StockAnalysis.Controllers
             return result;
         }
 
-        private async Task<float> GetStockPrice(string stockSymbol)
+        private float GetStockPrice(string stockSymbol)
         {
-            string url = @"http://download.finance.yahoo.com/d/quotes.csv?s=" + stockSymbol + "&f=a";
-            var result = await GetWebResponse(url);
+            //var result = GetWebResponse(YahooURL + stockSymbol + "&f=a");
             float stockPrice = 0;
-
-            using (StreamReader strm = new StreamReader(result.GetResponseStream()))
+            foreach (var line in ParseWebResponse(YahooURL + stockSymbol + "&f=a"))
             {
-                string line;
-                while ((line = strm.ReadLine()) != null)
-                {
-                    var lineInfo = line.Split(',');
-                    float.TryParse(lineInfo[0], out stockPrice);
-                }
+                var lineInfo = line.Split(',');
+                float.TryParse(lineInfo[0], out stockPrice);
             }
 
             return stockPrice;
         }
 
-        public ActionResult RemoveStock(string stockSymbol, bool stockSymbols)
+        public ActionResult RemoveStockSymbol(string stockSymbol)
         {
             // Remove stock from database
-            using (var userContext = new UsersContext())
+            PerformDatabaseAction((userContext, user) =>
             {
-                if (User?.Identity?.IsAuthenticated ?? true)
+                if (user.StockSymbols != null)
                 {
-                    string userName = User.Identity.Name;
-                    var user = userContext.GetUser(userName);
-                    if (user != null)
-                    {
-                        if (stockSymbols)
-                        {
-                            var list = Deserialize<List<string>>(user.StockSymbols);
-                            list.Remove(stockSymbol);
-                            user.StockSymbols = Serialize<List<string>>(list);
-                        }
-                        else
-                        {
-                            var list = Deserialize<List<UserStockModel>>(user.UserStockInformation);
-                            var stockToRemove = list.FirstOrDefault(i => i.StockSymbol == stockSymbol);
-                            if (stockToRemove != null)
-                            {
-                                list.Remove(stockToRemove);
-                                user.UserStockInformation = Serialize<List<UserStockModel>>(list);
-                            }
-                        }
+                    var list = Deserialize<List<string>>(user.StockSymbols);
+                    list.Remove(stockSymbol);
+                    user.StockSymbols = Serialize<List<string>>(list);
+                }
+            });
 
-                        userContext.Entry(user).CurrentValues.SetValues(user);
-                        userContext.SaveChanges();
+            return RedirectToAction("Stocks");
+        }
+
+        public ActionResult RemoveStockInformation(string stockSymbol)
+        {
+            // Remove stock from database
+            PerformDatabaseAction((userContext, user) =>
+            {
+                if (user.UserStockInformation != null)
+                {
+                    var list = Deserialize<List<UserStockModel>>(user.UserStockInformation);
+                    var stockToRemove = list.FirstOrDefault(i => i.StockSymbol == stockSymbol);
+                    if (stockToRemove != null)
+                    {
+                        list.Remove(stockToRemove);
+                        user.UserStockInformation = Serialize<List<UserStockModel>>(list);
                     }
                 }
-            }
+            });
 
             return RedirectToAction("Stocks");
         }
@@ -332,37 +289,34 @@ namespace StockAnalysis.Controllers
             return sb.ToString();
         }
 
-        private Task<HttpWebResponse> GetWebResponse(string url)
+        private HttpWebResponse GetWebResponse(string url)
         {
-            return Task<WebResponse>.Run(() =>
-            {
-                var webReq = (HttpWebRequest)WebRequest.Create(url);
-                var webResp = (HttpWebResponse)webReq.GetResponse();
-                return webResp;
-            });
+            var webReq = (HttpWebRequest)WebRequest.Create(url);
+            var webResp = (HttpWebResponse)webReq.GetResponse();
+            return webResp;
         }
         private bool ValidateStockSymbol(string stockSymbol)
         {
-            string url = @"http://download.finance.yahoo.com/d/quotes.csv?s=" + stockSymbol + "&f=sn";
-            var task = GetWebResponse(url);
+            foreach (var line in ParseWebResponse(YahooURL + stockSymbol + "&f=sn"))
+            {
+                // Return if the stock is not valid
+                if (line.Count() <= 1 || line.Split(',').Any(i => i.Contains("N/A")))
+                    return false;
+            }
 
-            // Add a stock to the table for user
-            using (StreamReader strm = new StreamReader(task.Result.GetResponseStream()))
+            return true;
+        }
+
+        private IEnumerable<string> ParseWebResponse(string url)
+        {
+            var result = GetWebResponse(url);
+            using (StreamReader strm = new StreamReader(result.GetResponseStream()))
             {
                 string line;
                 while ((line = strm.ReadLine()) != null)
                 {
-                    var lineInfo = line.Split(',');
-
-                    // Return if the stock is not valid
-                    if (line.Count() <= 1 || lineInfo.Any(i => i.Contains("N/A")))
-                    {
-                        //ModelState.AddModelError("StockNotFound", $"The stock symbol '{stockSymbol}' is not valid");
-                        return false;
-                    }
+                    yield return line;
                 }
-
-                return true;
             }
         }
 
@@ -388,6 +342,24 @@ namespace StockAnalysis.Controllers
                 }
 
                 return result;
+            }
+        }
+
+        private void PerformDatabaseAction(Action<UsersContext, UserProfile> action)
+        {
+            // Save result in database
+            using (var userContext = new UsersContext())
+            {
+                if (User?.Identity?.IsAuthenticated ?? true)
+                {
+                    var user = userContext.GetUser(User.Identity.Name);
+                    if (user != null)
+                    {
+                        action(userContext, user);
+                        userContext.Entry(user).CurrentValues.SetValues(user);
+                        userContext.SaveChanges();
+                    }
+                }
             }
         }
 
